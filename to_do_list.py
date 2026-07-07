a502291b04c391d4a8bda59b1f1f5e0c5c680bc4
FarhadAkbar <farhadakbar92@gmail.com>
2026-07-07 19:50:04 -0400

    Complete To-Do List

--- to_do_list.py
+++ to_do_list.py
@@ -1,6 +1,8 @@
 # A to do list
+
 def get_menu_choice():
 
+    print("Main Menu")
     print("1. Add Task")
     print("2. View Tasks")
     print("3. Remove Task")
@@ -17,4 +19,54 @@ def get_menu_choice():
 
     return choice
 
-get_menu_choice()
+def add_task(tasks):
+
+    task = input("Enter a task: ")
+
+    tasks.append(task)
+
+    print("Task added!")
+
+def view_tasks(tasks):
+    if len(tasks) == 0:
+        print("No tasks yet.")
+    else:
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
+
+def remove_task(tasks):
+    if len(tasks) == 0:
+        print("No tasks yet.")
+    else:
+        for index, task in enumerate(tasks, start=1):
+            print(f"{index}. {task}")
+
+        while True:
+            try:
+                task_to_delete = int(input("Enter the number of the task you want to delete: "))
+                if 1 <= task_to_delete <= len(tasks):
+                    break
+                print(f"Please enter a number between 1 and {len(tasks)}.")
+            except ValueError:
+                print("Error. Please enter a valid number of a task.")
+
+        tasks.pop(task_to_delete - 1)
+        print("Task removed!")
+
+def main():
+    tasks = []
+
+    while True:
+        choice = get_menu_choice()
+
+        if choice == 1:
+            add_task(tasks)
+        elif choice == 2:
+            view_tasks(tasks)
+        elif choice == 3:
+            remove_task(tasks)
+        elif choice == 4:
+            print("Goodbye!")
+            break
+
+main()
